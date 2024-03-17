64e1c6c8b1a75f8e423450c7df2918227130edfa
leohb07 <leohb1501@gmail.com>
2024-03-17 00:59:50 -0300

    feat: add new envs

--- src/shared/@types/env.d.ts
+++ src/shared/@types/env.d.ts
@@ -4,6 +4,12 @@ declare global {
       PORT?: string;
       GMAIL_APP_USER?: string;
       GMAIL_APP_PASSWORD?: string;
+      META_APP_ID?: string;
+      META_APP_SECRET?: string;
+      META_RECIPIENT_WAID?: string;
+      META_VERSION?: string;
+      META_PHONE_NUMBER_ID?: string;
+      META_ACCESS_TOKEN?: string;
     }
   }
 }
